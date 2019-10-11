993687bfb88f9b9b2ebd08cdd553b3026d158e47
chenxinyi <chenxinyi@chaintor.com>
2019-10-11 17:19:48 +0800

    react与electron整合

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+function createWindow () {   
+    require('devtron').install()
+    // 创建浏览器窗口
+    let mainWindow = new BrowserWindow({
+      width: 1024,
+      height: 680,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    const urlLocation = isDev ? 'http://localhost:3000' : 'dummyurl'
+    
+    mainWindow.loadURL(urlLocation)
+  }
+  
+  app.on('ready', createWindow)
